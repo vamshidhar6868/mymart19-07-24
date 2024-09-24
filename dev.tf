2c39e882fca60869c90091ebb3d4717aa85e74e7
vamshidhar6868 <165636001+vamshidhar6868@users.noreply.github.com>
2024-09-24 16:45:22 +0530

    Update dev.tf

--- dev.tf
+++ dev.tf
@@ -1,53 +1,5 @@
 provider "aws" {
   region = "ap-south-1"
-
-}
-resource "aws_security_group" "rds_sg" {
-  name = "rds_sg"
-  # Define ingress and egress rules for RDS
-  
- # ssh for terraform remote exec
-  ingress {
-    description = "Allow remote SSH from anywhere"
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-  }
-
-  # enable http
-  ingress {
-    description = "Allow HTTP request from anywhere"
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-  }
-
-  # enable http
-  ingress {
-    description = "Allow HTTP request from anywhere"
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-  }
-}
-
-resource "aws_db_instance" "my_rds_instance" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "8.0.35"
-  instance_class       = "db.t3.micro"
-  identifier           = "my-rds-instance"
-  db_name              = "mymart123"
-  username             = "admin"
-  password             = "admin123"
-  skip_final_snapshot  = true
-  publicly_accessible  = true
-
-  # Other configuration options (e.g., VPC, security groups, etc.)
 }
 resource "aws_instance" "name" {
   ami = "ami-0522ab6e1ddcc7055"
